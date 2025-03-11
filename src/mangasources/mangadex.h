101e837c44f5fe1866fbf459093ddc3e6cab84d3
UsernameDog <106203755+UsernameDog@users.noreply.github.com>
2025-03-11 22:29:51 +0000

    Update mangadex.h

--- src/mangasources/mangadex.h
+++ src/mangasources/mangadex.h
@@ -1,36 +1,11 @@
-#ifndef MANGADEX_H
-#define MANGADEX_H
+#ifndef MANGADEX_API_H
+#define MANGADEX_API_H
 
-#include <QSet>
+#include <QJsonObject>
+#include <QList>
+#include <QString>
 
-#include "abstractmangasource.h"
-#include "mangainfo.h"
+// Funkcja pobiera listę rozdziałów dla danej mangi w określonym języku
+QList<QJsonObject> fetchChapters(const QString &mangaId, const QString &language = "en");
 
-#define RAPIDJSON_ASSERT(x) \
-    if (!(x))               \
-        throw QException();
-
-#include "thirdparty/rapidjson.h"
-
-class MangaDex : public AbstractMangaSource
-{
-public:
-    explicit MangaDex(NetworkManager *dm);
-    virtual ~MangaDex() = default;
-
-    bool updateMangaList(UpdateProgressToken *token) override;
-    Result<MangaChapterCollection, QString> updateMangaInfoFinishedLoading(
-        QSharedPointer<DownloadStringJob> job, QSharedPointer<MangaInfo> info) override;
-    Result<QStringList, QString> getPageList(const QString &chapterUrl) override;
-
-private:
-    void login();
-    QString apiUrl;
-    QVector<QString> serverUrls;
-
-    QVector<QString> statuses;
-    QVector<QString> demographies;
-    QMap<int, QString> genreMap;
-};
-
-#endif  // MANGADEX_H
+#endif // MANGADEX_API_H
